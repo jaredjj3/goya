b2a2c2ec4df9e330e01e528fbb501db77be7a6d8
Jared Johnson <jared@nuts.com>
2018-12-15 14:33:42 -0500

    Implement side effect for Jared counter.

--- src/jared/components/counter/Counter.js
+++ src/jared/components/counter/Counter.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    }
+
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+  }
+
+  set count(count) {
+    const nextState = {
+      ...this.state,
+      count
+    };
+
+    this.setState(nextState);
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.count % 2 === 0) {
+      console.log(`prevState was : ${JSON.stringify(prevState)}`);
+    }
+  }
+
+  decrement(_event) {
+    this.count = this.state.count - 1;
+  }
+
+  increment(_event) {
+    this.count = this.state.count + 1;
+  }
+
   render() {
     return (
       <div>
-        Counter
+        <div>{this.props.title}</div>
+        {this.state.count}
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.increment}>+</button>
+        </div>
       </div>
     );
   }
--- src/sebas/components/counter/Counter.js
+++ src/sebas/components/counter/Counter.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+  }
+
+  decrement(_event) {
+    const newState = { 
+      ...this.state,
+      count: this.state.count - 1
+    }
+
+    this.setState(newState);
+  };
+
+  increment(_event) {
+    const newState = { 
+      ...this.state,
+      count: this.state.count + 1
+    }
+
+    this.setState(newState);
+  };
+
   render() {
     return (
       <div>
-        Sebas Counter
+        <button onClick={this.decrement}>-</button>        
+        {this.state.count}
+        <button onClick={this.increment}>+</button>
       </div>
     );
   }
