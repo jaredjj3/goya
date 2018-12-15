a9945f06c33f6459a2c371dfcbf3a83d802a7dbd
Jared Johnson <jared@nuts.com>
2018-12-15 12:08:27 -0500

    Clean up boilerplate and mount app for sebas and jared.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import JaredApp from './jared/App';
+import SebasApp from './sebas/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<JaredApp />, document.getElementById('jared'));
+ReactDOM.render(<SebasApp />, document.getElementById('sebas'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
