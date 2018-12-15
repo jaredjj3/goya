b78e5418c7040311bee805570f03bc8e8d2a2217
Jared Johnson <jared@nuts.com>
2018-12-15 16:59:08 -0500

    Separate hook logic in Sebas HookCounter component.

--- src/sebas/components/counter/HookCounter.js
+++ src/sebas/components/counter/HookCounter.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
-export function HookCounter(props) {
+const enhance = props => {
   const [count, setCount] = useState(props.initialCount || 0);
-  const increment = () => ( setCount(count + (props.factor || 1)) );
-  const decrement = () => ( setCount(count - (props.factor || 1)) );
+  const increment = () => (setCount(count + (props.factor || 1)));
+  const decrement = () => (setCount(count - (props.factor || 1)));
 
   useEffect(() => {
     if (count % 2 === 0) {
-      console.log('Value is divisible by 2!'); 
+      console.log('Value is divisible by 2!');
     }
   })
+  
+  return { count, increment, decrement };
+}
+
+export function HookCounter(props) {
+  const { count, increment, decrement } = enhance(props);
 
   return (
     <div>
