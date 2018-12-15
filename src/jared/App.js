5a6c63cc57f558b2809bff6c784e00cb0fcf521d
Jared Johnson <jared@nuts.com>
2018-12-15 12:32:41 -0500

    Create the Counter component.

--- src/jared/App.js
+++ src/jared/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Counter from './components/counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Counter />
       </div>
     );
   }
